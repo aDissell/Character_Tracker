05a49510014c71df4097d1bc51a27b2cb0ba6ac5
aDissell <akdissell@gmail.com>
2021-09-01 09:25:06 -0500

    Added ability to remove player from loaded list

--- player_class.py
+++ player_class.py
@@ -2,6 +2,7 @@ from mob_class import Mob
 
 
 class Player(Mob):
+    players = list()
     def __init__(self, name, hp, ac, stren, dex, con, wis, intel, chari):
         super().__init__(name=name, hp=hp, ac=ac, stren=stren, dex=dex, con=con, wis=wis, intel=intel, chari=chari)
 
--- test.py
+++ test.py
@@ -1,32 +1,37 @@
 from player_class import Player
 from npc_class import NPC
 
-players = list()
+#players = list()
 running = True
 
 while running:
     action = input("Welcome to Character Tracker.\nWhat would you like to do? : ")
     if action == "modify players":
         # Prompts user with the option to enter a new player
-        enter_new_player = input("Create, Remove, Update, Delete Players or Exit program :")
+        enter_new_player = input("Create, Remove, Update, Delete, Display Players or Exit program :").upper()
 
         # Initiates the input of a new player
-        if enter_new_player == "Add" or enter_new_player == "add":
+        if enter_new_player == "CREATE"
             player_input = input("Please enter a new player (name hp ac str dex con wis int cha) :")
-            new_player = Player.add_new(player_input, players)
+            new_player = Player.add_new(player_input, Player.players)
 
             # Adds new player to the list and 
             # displays the most recently added player
-            players.append(new_player)
-            print("You have added " + players[-1].name)
+            Player.players.append(new_player)
+            print("You have added " + Player.players[-1].name)
 
-        elif enter_new_player == "Remove" or enter_new_player == "remove":
-            pass
-        
-        elif enter_new_player == "Update" or enter_new_player == "update":
+        elif enter_new_player == "REMOVE":
+            print("This is the current list of players : ")
+            for player in Player.players:
+                print(Player.players.index(player))
+                player.my_name()
+            player_remove = int(input("Enter the index of the player to remove : "))
+            del Player.players[player_remove]
+
+        elif enter_new_player == "UPDATE":
             pass
 
-        elif enter_new_player == "Delete" or enter_new_player == "delete":
+        elif enter_new_player == "DELETE":
             pass
 
         else:
@@ -34,11 +39,8 @@ while running:
             # displays all players entered
             print("You have chosen to stop entering new players.")
             print("This is the current list of players : ")
-            for player in players:
+            for player in Player.players:
                 player.my_name()
                 player.my_hp()
             print('')
             action = input("What would you like to do? : ")
-
-
-        running = False
